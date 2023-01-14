7af7c292e7fa913f03893ba9d93fba7c6b5cd67f
Amir Imam Mohamed <ai_1583@hotmail.com>
2023-01-14 21:27:49 +0200

    Add project files.

---
+++ ProductsApp.Server/Program.cs
@@ -0,0 +1,40 @@
+using ProductsApp.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson(opt =>
+{
+    opt.UseMemberCasing();
+    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<DataBase>();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("Policy", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors("Policy");
+app.Run();
